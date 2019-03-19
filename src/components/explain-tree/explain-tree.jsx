03d36bae41bff8e8f17550e0cc882eb425d0896a
Durran Jordan <durran@gmail.com>
2019-03-19 23:09:50 +0100

    Fix scrolling issue with expanded divs

--- src/components/explain-tree/explain-tree.jsx
+++ src/components/explain-tree/explain-tree.jsx
@@ -101,9 +101,7 @@ class ExplainTree extends Component {
       <div>
         <div
           className={classnames(styles['explain-tree'])}
-          style={{ height: this.props.height, width: this.props.width }}
-          ref={(inst) => { tree = inst; }}
-        >
+          ref={(inst) => { tree = inst; }}>
           {this.getStages()}
         </div>
       </div>
